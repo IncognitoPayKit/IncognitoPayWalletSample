0eb51c4ff4ed2543fb8dfe4dc01191f704a76a52
Andreas Reuter <andreasreuter@yahoo.com>
2020-11-23 14:49:24 +0100

    Incognito Pay button as a sample

--- IncognitoPayWalletSample/IncognitoPayWalletSample/ViewController.swift
+++ IncognitoPayWalletSample/IncognitoPayWalletSample/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import IncognitoPayKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    let incognitoPayButton = IncognitoPayButton(base: self)
+    let button = UIView()
+    button.addSubview(incognitoPayButton)
+    button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+    
+    let stackView = UIStackView(arrangedSubviews: [
+      button
+    ])
+    stackView.axis = .vertical
+    stackView.alignment = .fill
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.addSubview(stackView)
+    
+    stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
   }
-
-
 }
-
